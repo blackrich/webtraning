05f0cd6eb273b0d84a5f2153d393f0426a52605d
leejunho <junho8296@naver.com>
2023-05-02 16:32:43 +0900

    20230502

--- pj1/jsc/main.js
+++ pj1/jsc/main.js
@@ -1,32 +1,45 @@
 let val = 0;
-const images = ["./src/login.png", "./src/logout.png","./src/person.png",  "./src/dog.jpg", "./src/logo.png"];
-// window.addEventListener('load', function() { _init(); });
+const images = ["./src/login.png", "./src/logout.png", "./src/person.png", "./src/dog.jpg", "./src/logo.png"];
+window.addEventListener('load', function () { _init(); });
 
-// function _init(){
-//   let result = document.getElementById("result");
-//   result.innerHTML = "";
-//   result.innerHTML = val;
-// }
+function _init() {
+  _previmagefunc(images.length - 1);
+  _mainimagefunc(0);
+  _nextimagefunc(1);
+}
 
-function increase(){
-//   let result = document.getElementById("result");
-//   result.innerHTML = "";
-val++;
-if(val >= images.length) val=0;
-// result.innerHTML = val;
-// result.innerHTML += val;
-_imagefunc(val);
+function increase() {
+  let result = document.getElementById("result");
+  _previmagefunc(val);
+  val++;
+  if (val >= images.length) val = 0;
+  _mainimagefunc(val);
+  let tmp = val + 1;
+  if (tmp >= images.length) tmp = 0;
+  _nextimagefunc(tmp);
 }
-function decrease(){
-// let result = document.getElementById("result");
-// result.innerHTML = "";
-val--;
-if(val < 0) val=images.length-1;
-// result.innerHTML = val;
-// result.innerHTML += val;
-_imagefunc(val);
+
+function decrease() {
+  let result = document.getElementById("result");
+  _nextimagefunc(val);
+  val--;
+  if (val < 0) val = images.length - 1;
+  _mainimagefunc(val);
+  let tmp = val - 1;
+  if (tmp < 0) tmp = images.length - 1;
+  _previmagefunc(tmp);
 }
-function _imagefunc(i){
-let imgoutput = document.getElementById("img-output");
-imgoutput.src=images[i];
+
+
+function _previmagefunc(i) {
+  let imgoutputmain = document.getElementById("img-output-prev");
+  imgoutputmain.src = images[i];
+}
+function _mainimagefunc(i) {
+  let imgoutputmain = document.getElementById("img-output-main");
+  imgoutputmain.src = images[i];
+}
+function _nextimagefunc(i) {
+  let imgoutputmain = document.getElementById("img-output-next");
+  imgoutputmain.src = images[i];
 }
